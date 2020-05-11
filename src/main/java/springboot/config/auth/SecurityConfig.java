a032a64969842c1b7e82fd707d24622d26ae7bcf
wlswhr160 <wlswhr160@naver.com>
2020-05-11 13:39:36 +0900

    /profile 추가

--- src/main/java/springboot/config/auth/SecurityConfig.java
+++ src/main/java/springboot/config/auth/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				// URL별로 권한 관리를 지정
 				.authorizeRequests()
 				// 권한 관리 대상을 지정
-				.antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+				.antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**","/profile").permitAll()
 				.antMatchers("/api/v1/**").hasRole(Role.USER.name())
 				// 설정된 값을 제외한 나머지에 대해서는 인증 받은 사용자만 허용
 				.anyRequest().authenticated()
---
+++ src/main/java/springboot/web/ProfileController.java
@@ -0,0 +1,26 @@
+package springboot.web;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+	private final Environment env;
+	
+	@GetMapping("/profile")
+	public String profile() {
+		// env.getActiveProfiles() : 현재 실행 중인 ActiveProfile을 모두 가져옴
+		List<String> profiles =Arrays.asList(env.getActiveProfiles());
+		String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+		List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+		return profiles.parallelStream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
+	}
+
+}
