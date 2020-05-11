bb99b41695066509db264c03018a95b00aaf5428
wlswhr160 <wlswhr160@naver.com>
2020-05-11 15:17:09 +0900

    무중단 배포 테스트

---
+++ scripts/profile.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# 쉬고 있는 프로파일 찾기
+function find_idle_profile()
+{
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8081/profile)
+    
+    if [ ${RESPONSE_CODE} -ge 400 ]
+    then
+        CURRENT_PROFILE=real2
+    else
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
+    
+    if [ ${CURRENT_PROFILE} == real1 ]
+    then
+        IDLE_PROFILE=real2
+    else
+        IDLE_PROFILE=real1
+    fi
+    
+    echo "${IDLE_PROFILE}"
+}
+
+# 쉬고 있는 포트 검색
+function find_idle_port()
+{
+    IDLE_PROFILE=$(find_idle_profile)
+    
+    if [ ${IDLE_PROFILE} == real1 ]
+    then
+        echo "8081"
+    else
+        echo "8082"
+    fi
+}
---
+++ scripts/start.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+REPOSITY=/home/ec2-user/app/step3
+PROJECT_NAME=springboot-webservice
+
+echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/zip/*.war $REPOSITORY/"
+cp $REPOSITORY/zip/*.war $REPOSITORY/
+
+WAR_NAME=$(ls -tr $REPOSITORY/*.war | tail -n 1)
+echo "> WAR FILE Name: $WAR_NAME"
+
+echo "> 새 애플리케이션($JAR_NAME) 배포"
+nohup java -jar \
+   -Dspring.config.location=classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=$IDLE_PROFILE \
+   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
