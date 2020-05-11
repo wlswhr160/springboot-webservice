70de8c4283a18028089bfdd04027ff9aa0b9d8fb
wlswhr160 <wlswhr160@naver.com>
2020-05-11 17:21:55 +0900

    header, start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,19 +4,19 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITY=/home/ec2-user/app/step3
+REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=springboot-webservice
 
-cd $REPOSITORY
+cd $REPOSITORY 
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.war $REPOSITORY/"
 cp $REPOSITORY/zip/*.war $REPOSITORY/
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.war | tail -n 1)
-echo "> WAR FILE Name: $WAR_NAME"
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep war | tail -n 1)
+IDLE_PROFILE=$(find_idle_profile)
 
 echo "> 새 애플리케이션($JAR_NAME) 배포"
 nohup java -jar \
-   -Dspring.config.location=classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=$IDLE_PROFILE \
-   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+	-Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=$IDLE_PROFILE \
+	$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
