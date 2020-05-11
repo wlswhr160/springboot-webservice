28d7bcbf60dc5bd6c91cf2c382cb7a3f8f9ed6ad
wlswhr160 <wlswhr160@naver.com>
2020-05-11 17:39:14 +0900

    profile.sh 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 쉬고 있는 프로파일 찾기
-function find_idle_profile()
+function find_idle_profile_old()
 {
 	RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8081/profile)
 	
@@ -23,7 +23,7 @@ function find_idle_profile()
 }
 
 # 쉬고 있는 포트 검색
-function find_idle_port()
+function find_idle_port_old()
 {
 	IDLE_PROFILE=$(find_idle_profile)
 	
@@ -33,4 +33,24 @@ function find_idle_port()
 	else
 		echo "8082"
 	fi
+}
+
+function find_idle_port {	
+	PORT=$(cat /etc/nginx/conf.d/service-url.inc | grep 8081)
+	if [ $PORT == 8081 ] 
+	then 
+	    echo "8082"
+	else 
+	    echo "8081"
+	fi
+}
+
+function find_idle_profile() {
+	IDLE_PORT=$(find_idle_port)
+	if [ $IDLE_PORT == 8081 ]
+	then
+		echo "real1"
+	else 
+		echo "real2"
+	fi	
 }
