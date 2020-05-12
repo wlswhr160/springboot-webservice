05cde753db563fbae62beb7eed3f0084c3f527d4
wlswhr160 <wlswhr160@naver.com>
2020-05-12 10:26:04 +0900

    health.sh 수정

--- scripts/health.sh
+++ scripts/health.sh
@@ -10,7 +10,6 @@ IDLE_PORT=$(find_idle_port)
 echo "> Health Check Start !!!"
 echo "> IDLE_PORT : $IDLE_PORT"
 echo "> curl -s http://localhost:$IDLE_PORT/profile"
-sleep 10
 
 for RETRY_COUNT in {1..10}
 do
